3e4379909927f2677ca357f8f7106bfe94713284
Arny-Daniel <146500044+Arny-Daniel@users.noreply.github.com>
2025-05-05 17:11:43 -0500

    ok?

--- database/database.js
+++ database/database.js
@@ -1,15 +1,15 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
 const sequelize = new Sequelize(
-  process.env.MYSQLDATABASE,
-  process.env.MYSQLUSER,
-  process.env.MYSQLPASSWORD,
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    host: process.env.MYSQLHOST,
-    dialect: 'mysql',
-    port: process.env.MYSQLPORT || 3306,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT
   }
 );
 
 module.exports = sequelize;
-
